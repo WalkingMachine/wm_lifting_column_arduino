e725c3b391b19a64795cd87916ae8ad589850dcb
Jeffrey Cousineau <jeffrey.cousineau@gmail.com>
2019-01-20 16:54:34 -0500

    Add set max topic

--- wm_lifting_column_arduino.ino
+++ wm_lifting_column_arduino.ino
@@ -13,6 +13,7 @@ const int pin_m1 = 5;
 const int pin_m2 = 6;
 const int pin_Btn_UP = 8;
 const int pin_Btn_DN = 9;
+const int max_value = 2387;
 
 volatile long position_count = 0;
 
@@ -40,14 +41,21 @@ void set_state(const std_msgs::Int32& value)
   state_pub.publish( &actuator_state );
 }
 
-void set_zero(const std_msgs::Int32& value)
+void set_zero()
 {
   position_count = 0;
+  position_value.data = position_count;
+}
+void set_max()
+{
+  position_count = max_value;
+  position_value.data = position_count;
+
 }
 
 ros::Subscriber<std_msgs::Int32> sub_cmd("/column/cmd", set_state );
 ros::Subscriber<std_msgs::Empty> sub_zero("/column/setZero", set_zero );
-
+ros::Subscriber<std_msgs::Empty> sub_max("/column/setMax", set_max );
 
 void setup() 
 {
@@ -56,6 +64,7 @@ void setup()
   nh.advertise(state_pub);
   nh.subscribe(sub_cmd);
   nh.subscribe(sub_zero);
+  nh.subscribe(sub_max);
 
   pinMode(pin_m1, OUTPUT);
   pinMode(pin_m2, OUTPUT);
@@ -79,13 +88,27 @@ void loop()
   Serial.println(mm);
 
   accu = 0;
-  if(digitalRead(pin_Btn_UP) == LOW || actuator_state.data == UP)
+  if(digitalRead(pin_Btn_UP) == LOW)
   {
+    actuator_state.data = STOP; // override topic if btn is used
     digitalWrite(pin_m1, HIGH);
     digitalWrite(pin_m2, LOW);
   }
-  else if(digitalRead(pin_Btn_DN) == LOW  || actuator_state.data == DOWN)
+  else if(digitalRead(pin_Btn_DN) == LOW)
   {
+    actuator_state.data = STOP; // override topic if btn is used
+    digitalWrite(pin_m1, LOW);
+    digitalWrite(pin_m2, HIGH);
+  }
+  else if(actuator_state.data == UP)
+  {
+    actuator_state.data = UP; // override topic if btn is used
+    digitalWrite(pin_m1, HIGH);
+    digitalWrite(pin_m2, LOW);
+  }
+  else if(actuator_state.data == DOWN)
+  {
+    actuator_state.data = DOWN; // override topic if btn is used
     digitalWrite(pin_m1, LOW);
     digitalWrite(pin_m2, HIGH);
   }
